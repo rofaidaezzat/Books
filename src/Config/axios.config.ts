05e36d0fce4952382958b59f54f8ddd504fa3771
rofaidaezzat <rofaidaezzat@gmail.com>
2026-01-08 15:55:17 +0200

    fi

--- src/Config/axios.config.ts
+++ src/Config/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "https://lavishly-fogless-sang.ngrok-free.dev/",
+    baseURL: "https://api-knowledge-think.vercel.app/",
     timeout: 180000,
     headers: {
         'Content-Type': 'application/json',
